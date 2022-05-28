8fc2f0eafeded30d5f2342fbbe763e0d41c1df3e
张志阵 <1809642355@qq.com>
2022-05-28 19:19:42 +0800

    feat:集合了多网站的快捷搜索功能

---
+++ index.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         快捷搜索
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.npmjs.com/*
+// @match        http://portal.ai.babytree-inc.com/*
+// @match        https://www.bootcdn.cn/*
+// @match        https://www.bilibili.com/*
+// @match        https://search.bilibili.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  document.onkeydown = function (event) {
+    var e = event || window.event;
+    console.log(e, e.keyCode);
+    if (e && e.metaKey && e.keyCode == 71) {
+      // meta + g
+      e.preventDefault();
+      if (window.location.origin.includes('www.npmjs.com')) {
+        // npmjs.com的搜索框定位
+        document.querySelector('input[type=\'search\']').focus()
+      }
+      if (window.location.origin.includes('www.bootcdn.cn')) {
+        // www.bootcdn.cn的搜索框定位
+        document.querySelector('.search-wraper input[type=\'text\']').focus()
+      }
+      if (window.location.origin.includes('www.bilibili.com')) {
+        // bilibili首页的搜索框定位
+        document.querySelector('.nav-search-content input').focus()
+      }
+
+      if (window.location.origin.includes('search.bilibili.com')) {
+        // bilibili搜索页的搜索框定位
+        document.querySelector('.search-input-el').focus()
+      }
+    }
+    if (window.location.origin.includes('portal.ai.babytree-inc.com')) {
+      if (e && e.metaKey && (e.keyCode == 71 || e.keyCode == 70)) {
+        e.preventDefault();
+        // bbt埋点系统的搜索框定位
+        document.querySelector("#search_content").focus()
+      }
+      if (e && e.metaKey && e.keyCode == 13) {
+        // meta（cmd/windows按键） + 回车
+        document.querySelector("body > div.container-wrap.mb-g > div.vip-right > div.right-box > div.am-titlebar.am-titlebar-default.am-no-layout > nav > form > button:nth-child(9)").click();
+      }
+      if (e && e.metaKey && e.keyCode == 86) {
+        //  // meta（cmd/windows按键） + c  点击复制后直接调起筛选
+        //document.querySelector("body > div.container-wrap.mb-g > div.vip-right > div.right-box > div.am-titlebar.am-titlebar-default.am-no-layout > nav > form > button:nth-child(9)").click();
+      }
+    }
+  }
+})();
