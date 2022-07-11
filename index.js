b8401a07e8fbd1dbaa37025b27db0ebc9fcfd11a
张志阵 <1809642355@qq.com>
2022-07-11 14:29:33 +0800

    feat:添加react docs搜索

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // @name:zh-CN   快捷搜索
 // @name         quickly search
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  mobile.ant.mobile、掘金、npmjs、bilibibli、bootstracpCDN、splunk、google API 快捷搜索，更多快捷搜索
 // @license      MIT
 // @author       zzailianlian
@@ -22,6 +22,7 @@
 // @match        http://gitlab.babytree-inc.com/*
 // @match        https://gitlab.babytree-inc.com/*
 // @match        https://hellogithub.com/*
+// @match        https://react.docschina.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
 // @grant        none
 // ==/UserScript==
@@ -97,7 +98,13 @@
           }, 500);
         }
       }
-
+      // react zh docs 
+      if (window.location.origin.includes('react.docschina.org')) {
+        const reactSearch = document.querySelector('#algolia-doc-search')
+        if (reactSearch) {
+          reactSearch.focus()
+        }
+      }
     }
     // cmd + enter
     if (e && e.metaKey && e.keyCode == 13) {
